df3b3aee4058f29620864d19b4ad103ea00ec47a
Cal <cydoval@mac.com>
2025-07-28 18:35:44 -0700

    services

--- src/app/components/pageheader.tsx
+++ src/app/components/pageheader.tsx
@@ -17,7 +17,7 @@ export default function PageHeader({
           {subtitle && <h4>{subtitle}</h4>}
           <h1>{title}</h1>
           {patternClassName && (
-            <div className={`${patternClassName} w-full h-15 mt-4`} />
+            <div className={`${patternClassName} w-full h-15 mt-12`} />
           )}
         </div>
       </div>
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,7 @@
 import PageHeader from "../components/pageheader";
+import Image from "next/image";
 
-export default function services() {
+export default function Services() {
   return (
     <>
       <PageHeader
@@ -10,6 +11,7 @@ export default function services() {
       />
       <div className="container">
         <div className="flex flex-col md:flex-row gap-2">
+          {/* Left Col */}
           <div className="md:w-1/2 w-full">
             <h2 className="mb-2">Help Desk/Infrastructure</h2>
             <p>
@@ -22,30 +24,99 @@ export default function services() {
               providing computer security consulting and remediation for the
               college under the Cyber-Safety Program.
             </p>
-            <div className="avatar-group -space-x-6">
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+            <div className="flex avatar-group -space-x-2 mt-2">
+              {[
+                { name: "Dj", src: "/team/dj.png" },
+                { name: "Surina", src: "/team/surina.png" },
+                { name: "Jackie", src: "/team/jackie.png" },
+                { name: "Steven", src: "/team/steven.png" },
+                { name: "Shuka", src: "/team/shuka.png" },
+              ].map(({ name, src }) => (
+                <div key={name} className="avatar">
+                  <div className="w-8">
+                    <Image
+                      src={src}
+                      alt={`${name}'s avatar`}
+                      width={48}
+                      height={48}
+                      role="img"
+                      aria-label={`${name}'s avatar`}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
+              ))}
+            </div>
+
+            <h2 className="mb-2 mt-5">Business Application Development</h2>
+            <p>
+              The Application Development group writes systems to automate
+              critical business functions. The group retains close ties with
+              application users, so many of the systems evolve continually to
+              address new needs or to meet changes (such as updates in DaFIS
+              accounting databases). The Application group authored and
+              maintains business applications such as Faculty Recruitments,
+              Pre-Purchasing, Student Information Management System, and
+              internal time reporting systems, to name a few. View our portfolio
+              to see our entire catalog of projects.
+            </p>
+            <div className="flex avatar-group -space-x-2 mt-2">
+              {[
+                { name: "Spruce", src: "/team/spruce.png" },
+                { name: "Rob", src: "/team/rob.png" },
+                { name: "Jason", src: "/team/jason.png" },
+                { name: "Scott", src: "/team/scott.png" },
+              ].map(({ name, src }) => (
+                <div key={name} className="avatar">
+                  <div className="w-8">
+                    <Image
+                      src={src}
+                      alt={`${name}'s avatar`}
+                      width={48}
+                      height={48}
+                      role="img"
+                      aria-label={`${name}'s avatar`}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://img.daisyui.com/images/profile/demo/averagebulk@192.webp" />
+              ))}
+            </div>
+
+            <h2 className="mb-2 mt-5">Web Design and Development</h2>
+            <p>
+              The web development group has nearly 40 websites in active use
+              and/or development. These can be divided into the following types:
+              Center sites, program and group sites, research focused sites,
+              institute sites, and conference/event sites. Sites include the
+              college homepage, Agricultural Sustainability Institute, Afghan
+              Agricultural Project, Foods for Health Initiative, as examples.
+            </p>
+            <div className="flex avatar-group -space-x-2 mt-2">
+              {[{ name: "Cal", src: "/team/cal.png" }].map(({ name, src }) => (
+                <div key={name} className="avatar">
+                  <div className="w-8">
+                    <Image
+                      src={src}
+                      alt={`${name}'s avatar`}
+                      width={48}
+                      height={48}
+                      role="img"
+                      aria-label={`${name}'s avatar`}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://img.daisyui.com/images/profile/demo/wonderperson@192.webp" />
-                </div>
-              </div>
+              ))}
             </div>
           </div>
-          <div className="md:w-1/2 w-full">33%</div>
+
+          {/* Right Col */}
+          <div className="md:w-1/2 w-full">
+            <div className="bg-base-100 p-4 shadow rounded">
+              <h3 className="text-lg font-semibold mb-2">33% Column</h3>
+              <p className="text-sm">
+                You can place sidebar or summary content here.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
